3e0a23d8d3da007750ba52943dc60e61cc1e5714
test user <owner@test.com>
2024-01-16 19:58:57 +0100

    Added the masked less than/equal check and the functions that it needs

--- ref/masking_gadgets.h
+++ ref/masking_gadgets.h
@@ -4,6 +4,9 @@
 #define MASKING_ORDER 3
 typedef struct Masked {uint16_t shares[MASKING_ORDER+1];} Masked;
 
+// Only used in the LEQ function, didn't have the time to make this work with the Masked we already had.
+typedef struct CompMasked {uint32_t shares[MASKING_ORDER+1];} CompMasked;
+
 void masked_binomial_dist(Masked* a, Masked* x, Masked* y, int k);
 void arith_refresh(Masked* x);
 void opti_B2A(Masked* y, Masked* x, int k);
